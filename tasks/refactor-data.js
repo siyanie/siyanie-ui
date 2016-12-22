c8585a52103b67b9fcd326a3d319212cf464a29b
alanevEugene <e@alanev.ru>
2016-12-22 22:01:01 +0300

    Refactor data (add bg names)

--- tasks/refactor-data.js
+++ tasks/refactor-data.js
@@ -1,41 +1,21 @@
-const yaml = require('js-yaml')
-
-const readFile = require('./tasks/utils/readFile')
-const writeFile = require('./tasks/utils/writeFile')
+const readFile = require('./utils/readFile')
+const writeFile = require('./utils/writeFile')
 const { assign } = Object
-const file = 'about'
+const file = 'services'
 
-readFile(`data/${file}.yml`)
+readFile(`data/${file}.json`)
 	.then(DATA => {
-		const { subsections: data, name } = yaml.safeLoad(DATA)
+		const { subsections: data, name } =JSON.parse(DATA)
 
 		for(let key in data) {
 			const { content } = data[key]
-			content.map(item => {
-				const { quote, author, text } = item
+			content.map((item, index) => {
+				const { bg } = item
+				if (!bg)
+					assign(item, {
+						bg: `${file}_${key}_${index + 1}.jpg`
+					})
 
-				if (text || text === null) {
-					assign(item, {
-						main: {
-							type: 'text',
-							content: text
-						}
-					})
-					delete item.text
-				}
-				if (quote) {
-					assign(item, {
-						quote: {
-							text: quote
-						}
-					})
-					if (author || author === null) {
-						assign(item.quote, {
-							author
-						})
-						delete item.author
-					}
-				}
 				return item
 			})
 		}
